e1577aaf3907b87b6d12b85097218c0aacd45084
Ambulong <zeng.ambulong@gmail.com>
2018-10-17 02:39:04 +0800

    u

--- vsplatego.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget -O /webgoat-server.jar https://test-1251457471.cosgz.myqcloud.com/webgoat-server-8.0.0.M17.jar;
-nohup java -jar /webgoat-server.jar --server.port=80 --server.address=0.0.0.0 >/dev/null 2>&1 &
